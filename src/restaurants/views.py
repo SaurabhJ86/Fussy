d791e695b20746f2eb138fe0b82955c8f85a2b20
Saurabh Jhingan <saurabh.jhingan.08@gmail.com>
2018-08-12 10:49:10 +0530

    Using Template Inheritance

--- src/restaurants/views.py
+++ src/restaurants/views.py
@@ -7,3 +7,14 @@ def home(request):
 
 	context = {"hello":"saurabh"}
 	return render(request,'home.html',context)
+
+
+def about(request):
+
+	context = {}
+	return render(request,'about.html',context)
+
+def contact(request):
+
+	context = {}
+	return render(request,'contact.html',context)
