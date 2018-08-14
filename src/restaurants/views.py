2fef61f80576e48b3560a4010b282d845029aab8
Saurabh Jhingan <saurabh.jhingan.08@gmail.com>
2018-08-14 08:13:20 +0530

    Template Based Views Added

--- src/restaurants/views.py
+++ src/restaurants/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.views import View
+from django.views.generic.base import TemplateView
 # Create your views here.
 
 def home(request):
@@ -9,19 +10,34 @@ def home(request):
 	return render(request,'home.html',context)
 
 
-def about(request):
+class HomeView(TemplateView):
 
-	context = {}
-	return render(request,'about.html',context)
+	template_name = "home.html"
 
-def contact(request):
+	def get_context_data(self,**kwargs):
+		context = super().get_context_data(**kwargs)
+		context["hello"] = "Saurabh"
+		return context
 
-	context = {}
-	return render(request,'contact.html',context)
+# Using the below two as Template Based Views in the url's file.
+# def about(request):
 
-class ContactView(View):
+# 	context = {}
+# 	return render(request,'about.html',context)
 
-	def get(self,request, *args, **kwargs):
+# def contact(request):
 
-		context = {}
-		return render(request, 'contact.html',context)
+# 	context = {}
+# 	return render(request,'contact.html',context)
+
+# Not using CBV
+# class ContactView(View):
+
+# 	def get(self,request, *args, **kwargs):
+
+# 		context = {}
+# 		return render(request, 'contact.html',context)
+
+# Although this is Template Based View, but using it in url's is more efficient
+# class ContactTemplateView(TemplateView):
+# 	template_name = "contact.html"
