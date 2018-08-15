dfcc0a20560562f30b3bf5a5a4ea09d853eed94c
Saurabh Jhingan <saurabh.jhingan.08@gmail.com>
2018-08-15 10:54:32 +0530

    Used ListView for Listing Restaurants

--- src/restaurants/views.py
+++ src/restaurants/views.py
@@ -2,6 +2,8 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.views import View
 from django.views.generic.base import TemplateView
+from django.views.generic.list import ListView
+from django.db.models import Q
 # Create your views here.
 
 from .models import RestaurantLocation
@@ -10,5 +12,34 @@ def restaurant_listView(request):
 
 	template = 'restaurants/restaurants_list.html'
 	queryset = RestaurantLocation.objects.all()
-	context = {"restaurants":queryset}
+	context = {"object_list":queryset}
 	return render(request,template,context)
+
+
+class RestaurantListView(ListView):
+	def get_queryset(self):
+		slug = self.kwargs.get("slug")
+		if slug:
+			queryset = RestaurantLocation.objects.filter(
+				Q(category__iexact=slug)|
+				Q(category__icontains=slug)
+				)
+		else:
+			queryset = RestaurantLocation.objects.all()
+
+		return queryset
+
+
+# class SearchRestaurantListView(ListView):
+# 	template_name = 'restaurants/restaurants_list.html'
+
+# 	def get_queryset(self):
+# 		slug = self.kwargs.get("slug")
+# 		if slug:
+# 			queryset = RestaurantLocation.objects.filter(
+# 				Q(category__iexact=slug)|
+# 				Q(category__icontains=slug)
+# 				)
+# 		else:
+# 			queryset = RestaurantLocation.objects.none()
+# 		return queryset
